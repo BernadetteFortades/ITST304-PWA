b038f6472e1fa0a432220eee4283c27f63b760fd
BernadetteFortades <130954177+BernadetteFortades@users.noreply.github.com>
2023-04-17 23:39:00 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,47 +1,45 @@
-var board = [];
-var currentPiece = null;
-var currentPieceType = null;
-var currentPiecePosition = null;
+var player = document.getElementById("player");
+var obstacles = document.getElementsByClassName("obstacle");
+var finish = document.getElementById("finish");
 
-function createPiece() {
-  // TODO: create a random Tetris piece
+function movePlayerLeft() {
+	var left = parseInt(player.style.left);
+	if (left > 0) {
+		player.style.left = (left - 50) + "px";
+	}
 }
 
-function movePieceDown() {
-  // TODO: move the current piece down by one block
+function movePlayerRight() {
+	var left = parseInt(player.style.left);
+	if (left < 750) {
+		player.style.left = (left + 50) + "px";
+	}
 }
 
-function movePieceLeft() {
-  // TODO: move the current piece left by one block
+function checkCollision() {
+	var playerRect = player.getBoundingClientRect();
+	for (var i = 0; i < obstacles.length; i++) {
+		var obstacleRect = obstacles[i].getBoundingClientRect();
+		if (playerRect.bottom >= obstacleRect.top &&
+			playerRect.top <= obstacleRect.bottom &&
+			playerRect.right >= obstacleRect.left &&
+			playerRect.left <= obstacleRect.right) {
+			alert("Game over!");
+			location.reload();
+		}
+	}
+	if (playerRect.bottom >= finish.getBoundingClientRect().top) {
+		alert("You win!");
+		location.reload();
+	}
 }
 
-function movePieceRight() {
-  // TODO: move the current piece right by one block
-}
-
-function rotatePiece() {
-  // TODO: rotate the current piece
-}
-
-function drawBoard() {
-  var gameBoard = document.getElementById("game-board");
-  gameBoard.innerHTML = "";
-  for (var y = 0; y < 20; y++) {
-    for (var x = 0; x < 10; x++) {
-      if (board[y][x] !== 0) {
-        var block = document.createElement("div");
-        block.className = "block";
-        block.style.top = (y * 20) + "px";
-        block.style.left = (x * 20) + "px";
-        gameBoard.appendChild(block);
-      }
-    }
-  }
-}
-
-function gameLoop() {
-  movePieceDown();
-  drawBoard();
-}
-
-setInterval(gameLoop, 1000);
+document.addEventListener("keydown", function(event) {
+	if (event.key === "ArrowLeft") {
+		movePlayerLeft();
+	}
+	else if (event.key === "ArrowRight") {
+		movePlayerRight();
+	}
+	checkCollision();
+});
