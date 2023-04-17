638eaaf16593fbaa4e1b2b4549baf2f496ab1ec1
BernadetteFortades <130954177+BernadetteFortades@users.noreply.github.com>
2023-04-17 23:36:22 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,79 +1,47 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+var board = [];
+var currentPiece = null;
+var currentPieceType = null;
+var currentPiecePosition = null;
 
-var cubeX = 200;
-var cubeY = 200;
-var cubeSize = 50;
-var cubeColor = "red";
-
-var leftPressed = false;
-var rightPressed = false;
-var upPressed = false;
-var downPressed = false;
-
-function drawCube() {
-	ctx.fillStyle = cubeColor;
-	ctx.fillRect(cubeX, cubeY, cubeSize, cubeSize);
+function createPiece() {
+  // TODO: create a random Tetris piece
 }
 
-function clearCanvas() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
-function moveCube() {
-	if (leftPressed && cubeX > 0) {
-		cubeX -= 5;
-	}
-	if (rightPressed && cubeX < canvas.width - cubeSize) {
-		cubeX += 5;
-	}
-	if (upPressed && cubeY > 0) {
-		cubeY -= 5;
-	}
-	if (downPressed && cubeY < canvas.height - cubeSize) {
-		cubeY += 5;
-	}
+function movePieceDown() {
+  // TODO: move the current piece down by one block
 }
 
-function keyDownHandler(e) {
-	if (e.keyCode == 37) {
-		leftPressed = true;
-	} else if (e.keyCode == 39) {
-		rightPressed = true;
-	} else if (e.keyCode == 38) {
-		upPressed = true;
-	} else if (e.keyCode == 40) {
-		downPressed = true
+function movePieceLeft() {
+  // TODO: move the current piece left by one block
+}
+
+function movePieceRight() {
+  // TODO: move the current piece right by one block
+}
+
+function rotatePiece() {
+  // TODO: rotate the current piece
+}
+
+function drawBoard() {
+  var gameBoard = document.getElementById("game-board");
+  gameBoard.innerHTML = "";
+  for (var y = 0; y < 20; y++) {
+    for (var x = 0; x < 10; x++) {
+      if (board[y][x] !== 0) {
+        var block = document.createElement("div");
+        block.className = "block";
+        block.style.top = (y * 20) + "px";
+        block.style.left = (x * 20) + "px";
+        gameBoard.appendChild(block);
+      }
+    }
   }
 }
-function keyDownHandler(e) {
-	if (e.keyCode == 37) {
-		leftPressed = true;
-	} else if (e.keyCode == 39) {
-		rightPressed = true;
-	} else if (e.keyCode == 38) {
-		upPressed = true;
-	} else if (e.keyCode == 40) {
-		downPressed = true;
-	}
-}
-function keyUpHandler(e) {
-	if (e.keyCode == 37) {
-		leftPressed = false;
-	} else if (e.keyCode == 39) {
-		rightPressed = false;
-	} else if (e.keyCode == 38) {
-		upPressed = false;
-	} else if (e.keyCode == 40) {
-		downPressed = false;
-	}
-}
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
 
 function gameLoop() {
-	clearCanvas();
-	moveCube();
-	drawCube();
+  movePieceDown();
+  drawBoard();
 }
 
-setInterval(gameLoop, 20);
+setInterval(gameLoop, 1000);
